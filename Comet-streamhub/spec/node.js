68e761bef8d7f09f4be5211f807af3404763bdf1
Aaron Unger <aunger@concord.org>
2010-03-04 11:19:27 -0500

    A few actual running tests.

--- Comet-streamhub/spec/node.js
+++ Comet-streamhub/spec/node.js
@@ -2,7 +2,7 @@
 require.paths.unshift('spec', '/opt/local/lib/ruby/gems/1.8/gems/jspec-3.3.2/lib', 'lib')
 require('jspec')
 require('unit/spec.helper')
-require('yourlib')
+require('biologica-library')
 
 JSpec
   .exec('spec/unit/spec.js')
--- Comet-streamhub/spec/unit/spec.js
+++ Comet-streamhub/spec/unit/spec.js
@@ -1,8 +1,21 @@
 
-describe 'YourLib'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
+describe 'Biologica Library'
+  describe '.getSelectedOrganisms(appletId)'
+    it 'should add an organism to the applet'
+			$(applets).append(fixture('multiple-organism-applet'))
+			applet = document.getElementById("multi-org-applet")
+      applet.addNewOrganism("test")
+    end
+    it 'should not get an organism'
+      orgs = getSelectedOrganisms("multi-org-applet")
+      orgs.should_not.be_null
+			orgs.length.should.be 0
+    end
+    it 'should get an applet'
+      $(applets).append(fixture('static-organism-applet'))
+      orgs = getSelectedOrganisms("static-org-applet")
+      orgs.should_not.be_null
+			orgs.length.should.be 1
     end
   end
 end
