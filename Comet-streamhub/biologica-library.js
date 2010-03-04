20676bf3a397aa24d5f1bc8f07e0fb11b7449824
Aaron Unger <aunger@concord.org>
2010-03-04 09:37:59 -0500

    Add "transfer as copies" demo.

--- Comet-streamhub/biologica-library.js
+++ Comet-streamhub/biologica-library.js
@@ -6,7 +6,25 @@ function addOrganisms(appletId, orgs){
 	document.getElementById(appletId).addOrganisms(orgs);
 }
 
+function addOrganismsAsCopies(appletId, orgs){
+	var orgCopies = copyOrganisms(orgs);
+	document.getElementById(appletId).addOrganisms(orgCopies);
+}
+
 function transferSelectedOrganisms(fromAppletId, toAppletId){
 	var orgs = getSelectedOrganisms(fromAppletId);
 	addOrganisms(toAppletId, orgs);
+}
+
+function transferSelectedOrganismsAsCopies(fromAppletId, toAppletId){
+	var orgs = getSelectedOrganisms(fromAppletId);
+	addOrganismsAsCopies(toAppletId, orgs);
+}
+
+function copyOrganisms(orgs) {
+	var orgCopies = new Array();
+	for (var i=0; i<orgs.length; i++) {
+		orgCopies.push(orgs[i].copy());
+	}
+	return orgCopies;
 }
